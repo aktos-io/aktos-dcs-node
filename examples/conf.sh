29c98bbf53cc49a24df9c1fcbc226d2b0a9672b2
Cerem Cem ASLAN <cem@aktos.io>
2017-07-04 23:58:46 +0300

    added new environment variable setter script

--- examples/conf.sh
+++ examples/conf.sh
@@ -5,7 +5,11 @@ find_scada_root () {
     testing_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     while :; do
         name=$(basename $testing_dir)
-        if [[ "$name" == "scada.js" ]]; then
+        ls "$testing_dir/scada.js" > /dev/null 2>&1
+        if [[ "$?" == "0" ]]; then
+            echo "$testing_dir/scada.js"
+            return 0
+        elif [[ "$name" == "scada.js" ]]; then
             echo $testing_dir
             return 0
         elif [[ "$name" == "/" ]]; then
@@ -17,6 +21,11 @@ find_scada_root () {
 }
 
 SCADA=$(find_scada_root)
+
+if [[ "$SCADA" == "" ]]; then 
+    echo "scada.js root can not be found. exiting."
+    exit 255
+fi
 
 # change working directory
 export NODE_PATH="${SCADA}/node_modules:${SCADA}/src/lib:$NODE_PATH"
